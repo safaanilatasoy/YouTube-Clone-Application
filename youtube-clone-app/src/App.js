dc1d91fdbd3c3333933cc2d7170f9deaacc7c40e
safaanilatasoy <safaanilatasoy@gmail.com>
2022-10-28 21:31:41 +0300

    main

--- youtube-clone-app/src/App.js
+++ youtube-clone-app/src/App.js
@@ -1,13 +1,17 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Box} from '@mui/material';
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
     <Box sx={{backgroundColor: '#000'}}>
-      Navbar
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/video/:id" element={<ChannelDetail />} />
+        <Route path="/video/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
 
